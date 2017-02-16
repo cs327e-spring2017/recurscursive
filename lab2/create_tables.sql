4bf01e3e437aaf25fd62f4cf51125ffd00b85bdb
khalidgit <khalid_hamza@utexas.edu>
2017-02-15 22:49:44 -0600

    Updated the create_tables.sql

--- lab2/create_tables.sql
+++ lab2/create_tables.sql
@@ -3,81 +3,84 @@ drop database if exists tickit;
 create database imbd;
 \c imbd;
 
-CREATE TABLE Keywords (  /*---parent---*/
-    id_keywords integer primary key,
-    keyword varchar(50) 
-);
-
 CREATE TABLE Actors ( 
     id_actors integer primary key,
-    l_name varchar(30),  
-    f_name varchar(30), 
-    m_name varchar(30), 
-    gender integer, 
-    id_genres integer
+    lname varchar(30),  
+    fname varchar(30), 
+    mname varchar(30), 
+    gender varchar(30), 
+    number integer 
 );
 
-CREATE TABLE Aka_names ( /*types not yet corrected*/
-    id_aka_names integer primary key,
-    id_actors integer,
-    name varchar(50) /*for now, check this data type*/
+CREATE TABLE Aka_names (
+    idaka_names integer primary key,	
+    idactors integer not null references Actors(idactors),
+    name varchar(50) 
 );
 
-CREATE TABLE Genres ( /*types not yet corrected*/
-    id_genres integer primary key,
-    genres varchar(30) /*for now, check this data type*/
+CREATE TABLE Genres (
+    idgenres integer primary key,
+    genre varchar(30)
 );
 
-CREATE TABLE Aka_titles ( /*types not yet corrected*/
-    id_aka_titles integer primary key,
-    id_movies integer, 
-    title varchar(50),
-    location varchar(50), /*for now, check this data type*/
-    year char(4)
-); 
-
-CREATE TABLE Cast ( /*types not yet corrected*/
-    id_cast integer primary key,
-    id_movies integer, /*for now, check this data type*/
-    id_series integer,
-    id_actors integer,
-    character varchar(100), /*for now, check this data type*/
-    billing_position integer
-);
-
-
-CREATE TABLE Movies ( /*types not yet corrected*/
-    id_movies integer primary key,
-    titles varchar(50), 
+CREATE TABLE Movies ( 
+    idmovies integer primary key,
+    titles varchar(100), 
     year char(4),
-    id_genres integer, /*name not yet corrected*/
+    number integer, 
     type integer,
     location varchar(50),
     language varchar(50)
 );
 
-CREATE TABLE Movies_genres ( /*types not yet corrected*/
-    id_movies_genres integer primary key,
-    id_movies integer,
-    id_genres integer,
-    id_series integer
-);
+CREATE TABLE Aka_titles ( 
+    idaka_titles integer primary key,
+    idmovies integer not null references Movies(idmovies), 
+    title varchar(50),
+    location varchar(50),
+    year char(4)
+); 
 
-CREATE TABLE Movies_keywords ( /*types not yet corrected*/
-    id_movies_keywords integer primary key,
-    id_movies integer,
-    id_keywords integer
-);
-
-CREATE TABLE Movies_info ( /*types not yet corrected*/
-    id_movies integer primary key,
-    id_movies_keywords text
-);
-
-CREATE TABLE Series ( /*types not yet corrected*/
-    id_series integer primary key,
-    id_movies integer,
-    name text,
+CREATE TABLE Series ( 
+    idseries integer primary key,
+    idmovies integer not null references Movies(idmovies),
+    name varchar(100),
     season integer,
-    id_genres integer
+    number integer
 );
+
+CREATE TABLE Movies_genres (
+    idmovies_genres integer primary key,
+    idmovies integer not null references Movies(idmovies),
+    idgenres integer not null references Genres(idgenres),
+    idseries integer not null references Series(idseries)
+);
+
+CREATE TABLE Keywords ( 
+    idkeywords integer primary key,
+    keyword varchar(50) 
+);
+
+
+CREATE TABLE Movies_keywords (
+    idmovies_keywords integer primary key,
+    idmovies integer not null references Movies(idmovies),
+    idkeywords integer not null references Keywords(idkeywords),
+    idseries integer
+);
+
+CREATE TABLE Movies_info (
+    idmovies_info integer primary key,
+    idmovies integer not null references Movies(idmovies),
+    idmovies_keywords integer not null references Movies_keywords(idmovies_keywords) 
+);
+
+CREATE TABLE Cast_ ( 
+    idcast integer primary key,
+    idmovies integer not null references Movies(idmovies), 
+    idseries integer not null references Series(idseries),
+    idactors integer not null references Actors(idactors),
+    character varchar(100), 
+    billing_position integer
+);
+
