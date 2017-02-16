40b55ed0c70ae4342528325bcabf432454279ec2
khalidgit <khalid_hamza@utexas.edu>
2017-02-15 23:38:22 -0600

    Adding load_data.sql file

---
+++ lab2/load_data.sql
@@ -0,0 +1,21 @@
+\c imdb;
+
+\copy Actors from 'c:/Users/Elias/full/actors.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+
+\copy Aka_titles from 'c:/Users/Elias/full/Aka_titles.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+
+\copy Genres from 'c:/Users/Elias/full/genres.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+
+\copy Aka_Names from 'c:/Users/Elias/full/aka_names.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+
+\copy Cast_ from 'c:/Users/Elias/full/cast.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+
+\copy Keywords from 'c:/Users/Elias/full/keywords.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+
+\copy Movies from 'c:/Users/Elias/full/movies.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+
+\copy Movies_Genres from 'c:/Users/Elias/full/movies_genres.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+
+\copy Movies_Keywords from 'c:/Users/Elias/full/movies_keywords.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+
+\copy Series from 'c:/Users/Elias/full/series.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
