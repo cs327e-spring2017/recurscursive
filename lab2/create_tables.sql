05cead7e1bc0715f2f9bd9f7918e502b4f0629d6
khalidgit <khalid_hamza@utexas.edu>
2017-02-15 23:53:15 -0600

    updating create_table.sql

--- lab2/create_tables.sql
+++ lab2/create_tables.sql
@@ -4,83 +4,83 @@ create database imdb;
 \c imdb;
 
 CREATE TABLE Actors ( 
-    idactors integer primary key,
+    idactors varchar(50) primary key,
     lname varchar(30),  
     fname varchar(30), 
     mname varchar(30), 
     gender varchar(30), 
-    number integer 
+    number varchar(50) 
 );
 
 CREATE TABLE Aka_names (
-    idaka_names integer primary key,	
-    idactors integer not null references Actors(idactors),
+    idaka_names varchar(50) primary key,	
+    idactors varchar(50) not null references Actors(idactors),
     name varchar(50) 
 );
 
 CREATE TABLE Genres (
-    idgenres integer primary key,
+    idgenres varchar(50) primary key,
     genre varchar(30)
 );
 
 CREATE TABLE Movies ( 
-    idmovies integer primary key,
+    idmovies varchar(50) primary key,
     titles varchar(100), 
     year char(4),
-    number integer, 
-    type integer,
+    number varchar(50), 
+    type varchar(50),
     location varchar(50),
     language varchar(50)
 );
 
 CREATE TABLE Aka_titles ( 
-    idaka_titles integer primary key,
-    idmovies integer not null references Movies(idmovies), 
+    idaka_titles varchar(50) primary key,
+    idmovies varchar(50) not null references Movies(idmovies), 
     title varchar(50),
     location varchar(50),
     year char(4)
 ); 
 
 CREATE TABLE Series ( 
-    idseries integer primary key,
-    idmovies integer not null references Movies(idmovies),
+    idseries varchar(50) primary key,
+    idmovies varchar(50) not null references Movies(idmovies),
     name varchar(100),
-    season integer,
-    number integer
+    season varchar(50),
+    number varchar(50)
 );
 
 CREATE TABLE Movies_genres (
-    idmovies_genres integer primary key,
-    idmovies integer not null references Movies(idmovies),
-    idgenres integer not null references Genres(idgenres),
-    idseries integer not null references Series(idseries)
+    idmovies_genres varchar(50) primary key,
+    idmovies varchar(50) not null references Movies(idmovies),
+    idgenres varchar(50) not null references Genres(idgenres),
+    idseries varchar(50) not null references Series(idseries)
 );
 
 CREATE TABLE Keywords ( 
-    idkeywords integer primary key,
+    idkeywords varchar(50) primary key,
     keyword varchar(50) 
 );
 
 
 CREATE TABLE Movies_keywords (
-    idmovies_keywords integer primary key,
-    idmovies integer not null references Movies(idmovies),
-    idkeywords integer not null references Keywords(idkeywords),
-    idseries integer
+    idmovies_keywords varchar(50) primary key,
+    idmovies varchar(50) not null references Movies(idmovies),
+    idkeywords varchar(50) not null references Keywords(idkeywords),
+    idseries varchar(50)
 );
 
 CREATE TABLE Movies_info (
-    idmovies_info integer primary key,
-    idmovies integer not null references Movies(idmovies),
-    idmovies_keywords integer not null references Movies_keywords(idmovies_keywords) 
+    idmovies_info varchar(50) primary key,
+    idmovies varchar(50) not null references Movies(idmovies),
+    idmovies_keywords varchar(50) not null references Movies_keywords(idmovies_keywords) 
 );
 
 CREATE TABLE Cast_ ( 
-    idcast integer primary key,
-    idmovies integer not null references Movies(idmovies), 
-    idseries integer not null references Series(idseries),
-    idactors integer not null references Actors(idactors),
+    idcast varchar(50) primary key,
+    idmovies varchar(50) not null references Movies(idmovies), 
+    idseries varchar(50) not null references Series(idseries),
+    idactors varchar(50) not null references Actors(idactors),
     character varchar(100), 
-    billing_position integer
+    billing_position varchar(50)
 );
 
---
+++ lab2/create_tables_integers.sql
@@ -0,0 +1,86 @@
+\c postgres;
+drop database if exists imdb;
+create database imdb;
+\c imdb;
+
+CREATE TABLE Actors ( 
+    idactors integer primary key,
+    lname varchar(30),  
+    fname varchar(30), 
+    mname varchar(30), 
+    gender varchar(30), 
+    number integer 
+);
+
+CREATE TABLE Aka_names (
+    idaka_names integer primary key,	
+    idactors integer not null references Actors(idactors),
+    name varchar(50) 
+);
+
+CREATE TABLE Genres (
+    idgenres integer primary key,
+    genre varchar(30)
+);
+
+CREATE TABLE Movies ( 
+    idmovies integer primary key,
+    titles varchar(100), 
+    year char(4),
+    number integer, 
+    type integer,
+    location varchar(50),
+    language varchar(50)
+);
+
+CREATE TABLE Aka_titles ( 
+    idaka_titles integer primary key,
+    idmovies integer not null references Movies(idmovies), 
+    title varchar(50),
+    location varchar(50),
+    year char(4)
+); 
+
+CREATE TABLE Series ( 
+    idseries integer primary key,
+    idmovies integer not null references Movies(idmovies),
+    name varchar(100),
+    season integer,
+    number integer
+);
+
+CREATE TABLE Movies_genres (
+    idmovies_genres integer primary key,
+    idmovies integer not null references Movies(idmovies),
+    idgenres integer not null references Genres(idgenres),
+    idseries integer not null references Series(idseries)
+);
+
+CREATE TABLE Keywords ( 
+    idkeywords integer primary key,
+    keyword varchar(50) 
+);
+
+
+CREATE TABLE Movies_keywords (
+    idmovies_keywords integer primary key,
+    idmovies integer not null references Movies(idmovies),
+    idkeywords integer not null references Keywords(idkeywords),
+    idseries integer
+);
+
+CREATE TABLE Movies_info (
+    idmovies_info integer primary key,
+    idmovies integer not null references Movies(idmovies),
+    idmovies_keywords integer not null references Movies_keywords(idmovies_keywords) 
+);
+
+CREATE TABLE Cast_ ( 
+    idcast integer primary key,
+    idmovies integer not null references Movies(idmovies), 
+    idseries integer not null references Series(idseries),
+    idactors integer not null references Actors(idactors),
+    character varchar(100), 
+    billing_position integer
+);
+
