76d43043d791c629ffca2d26311cfb2ea369c4ae
eaatx10 <elias.ansari@utexas.edu>
2017-02-17 00:43:08 -0600

    adding verify_data.sql file

---
+++ lab2/verify_data.sql
@@ -0,0 +1,28 @@
+\c postgres;
+\c imdb;
+
+SELECT COUNT(*) FROM Movies; /*Equal to length of row (From Piazza) + 1*/
+
+SELECT COUNT(*) FROM Actors; /*Equal to length of row (From Piazza) + 1*/
+
+SELECT COUNT(*) FROM Keywords; /*Equal to length of row (From Piazza) + 1*/
+
+SELECT COUNT(*) FROM Genres; /*Equal to length of row (From Piazza) + 1*/
+
+SELECT COUNT(*) FROM Aka_Names; /*Equal to length of row (From Piazza) + 1*/
+
+SELECT COUNT(*) FROM Aka_Titles; /*Equal to length of row (From Piazza) + 1*/
+
+SELECT COUNT(*) FROM Series; /*Equal to length of row (From Piazza) + 1*/
+
+SELECT COUNT(*) FROM Movies_Genres; /*Equal to length of row (From Piazza) + 1*/
+
+SELECT COUNT(*) FROM Movies_Keywords; /*Equal to length of row (From Piazza) + 1*/
+
+SELECT COUNT(*) FROM Cast_; /*Equal to length of row (From Piazza) + 1*/
+
+
+
+
+
+
