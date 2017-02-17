de71032e38b19709a870f8af179a3c989cb4eee2
khalidgit <khalid_hamza@utexas.edu>
2017-02-16 19:49:51 -0600

    correcting data

--- lab2/load_data.sql
+++ lab2/load_data.sql
@@ -1,21 +1,21 @@
 \c imdb;
 
-\copy Movies from 'c:/Users/Elias/full/movies.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+\copy Movies from 'c:/Users/Elias/Downloads/full1/movies.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
 
-\copy Actors from 'c:/Users/Elias/full/actors.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+\copy Actors from 'c:/Users/Elias/Downloads/full1/actors.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
 
-\copy Keywords from 'c:/Users/Elias/full/keywords.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+\copy Keywords from 'c:/Users/Elias/Downloads/full1/keywords.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
 
-\copy Genres from 'c:/Users/Elias/full/genres.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+\copy Genres from 'c:/Users/Elias/Downloads/full1/genres.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
 
-\copy Aka_Names from 'c:/Users/Elias/full/aka_names.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+\copy Aka_Names from 'c:/Users/Elias/Downloads/full1/aka_names.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
 
-\copy Aka_Titles from 'c:/Users/Elias/full/aka_titles.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+\copy Aka_Titles from 'c:/Users/Elias/Downloads/full1/aka_titles.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
 
-\copy Series from 'c:/Users/Elias/full/series.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+\copy Series from 'c:/Users/Elias/Downloads/full1/series.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+ 
+\copy Movies_Genres from 'c:/Users/Elias/Downloads/full1/movies_genres.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
 
-\copy Movies_Genres from 'c:/Users/Elias/full/movies_genres.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+\copy Movies_Keywords from 'c:/Users/Elias/Downloads/full1/movies_keywords.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
 
-\copy Movies_Keywords from 'c:/Users/Elias/full/movies_keywords.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
-
-\copy Cast_ from 'c:/Users/Elias/full/cast.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
+\copy Cast_ from 'c:/Users/Elias/Downloads/full1/cast.csv' (header FALSE, format csv, delimiter ',', encoding 'UTF8');
