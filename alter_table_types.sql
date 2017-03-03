9c2e4b06cfadfcd55daabc14a2114f41f8ed5968
khalidgit <khalid_hamza@utexas.edu>
2017-03-02 20:35:55 -0600

    adding alter_table_type file

--- alter_table_types.sql
+++ alter_table_types.sql
@@ -1,1 +1,6 @@
-alter_table_types
+\c postgres;
+\c imdb;
+
+/*ALTER TABLE the_table ALTER COLUMN col_name TYPE integer USING (col_name::integer);*/
+
+ALTER TABLE Movies ALTER COLUMN idmovies TYPE integer USING (col_name::integer);
