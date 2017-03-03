89c48283283c72560f229aa11b17b00d9a48e0fe
eaatx10 <elias.ansari@utexas.edu>
2017-03-02 21:14:46 -0600

    updated create_tables.sql with correct data types

--- alter_table_types.sql
+++ alter_table_types.sql
@@ -3,4 +3,12 @@
 
 /*ALTER TABLE the_table ALTER COLUMN col_name TYPE integer USING (col_name::integer);*/
 
-ALTER TABLE Movies ALTER COLUMN idmovies TYPE integer USING (col_name::integer);
+ALTER TABLE Aka_Titles DROP CONSTRAINT idmovies_fkey 
+
+ALTER COLUMN idmovies TYPE integer USING (idmovies::integer);
+
+ALTER TABLE Movies ALTER COLUMN idmovies TYPE integer USING (idmovies::integer);
+
+ALTER TABLE Movies ALTER COLUMN idmovies TYPE integer USING (idmovies::integer);
+
+ALTER TABLE Movies ALTER COLUMN idmovies TYPE integer USING (idmovies::integer);
--- lab2/create_tables.sql
+++ lab2/create_tables.sql
@@ -4,82 +4,74 @@ create database imdb;
 \c imdb;
 
 CREATE TABLE Movies ( 
-    idmovies varchar(50) primary key,
+    idmovies integer primary key,
     titles varchar(500), 
-    year char(4),
-    number varchar(50), 
+    year integer,
     type integer,
     location varchar(50),
     language varchar(50)
 );
 
 CREATE TABLE Actors ( 
-    idactors varchar(50) primary key,
+    idactors integer primary key,
     lname varchar(200),  
     fname varchar(100), 
     mname varchar(100), 
     gender integer, 
-    number varchar(50) 
 );
 
 CREATE TABLE Keywords ( 
-    idkeywords varchar(50) primary key,
+    idkeywords integer primary key,
     keyword varchar(50) 
 );
 
 CREATE TABLE Genres (
-    idgenres varchar(50) primary key,
+    idgenres integer primary key,
     genre varchar(30)
 );
 
 CREATE TABLE Aka_Names (
-    idaka_names varchar(50) primary key,	
-    idactors varchar(50) not null references Actors(idactors),
+    idaka_names integer primary key,	
+    idactors integer not null references Actors(idactors),
     name varchar(200) 
 );
 
 CREATE TABLE Aka_Titles ( 
-    idaka_titles varchar(50) primary key,
-    idmovies varchar(50) not null references Movies(idmovies), 
+    idaka_titles integer primary key,
+    idmovies integer not null references Movies(idmovies), 
     title varchar(200),
     location varchar(50),
-    year char(4)
+    year integer
 ); 
 
 CREATE TABLE Series ( 
-    idseries varchar(50) primary key,
-    idmovies varchar(50) not null references Movies(idmovies),
+    idseries integer primary key,
+    idmovies integer not null references Movies(idmovies),
     name varchar(300),
     season varchar(50),
     number varchar(50)
 );
 
 CREATE TABLE Movies_Genres (
-    idmovies_genres varchar(50) primary key,
-    idmovies varchar(50) not null references Movies(idmovies),
-    idgenres varchar(50) not null references Genres(idgenres),
-    idseries varchar(50) references Series(idseries)
+    idmovies_genres integer primary key,
+    idmovies integer not null references Movies(idmovies),
+    idgenres integer not null references Genres(idgenres),
+    idseries integer references Series(idseries)
 );
 
 
 CREATE TABLE Movies_Keywords (
-    idmovies_keywords varchar(50) primary key,
-    idmovies varchar(50) not null references Movies(idmovies),
-    idkeywords varchar(50) not null references Keywords(idkeywords),
-    idseries varchar(50) 
+    idmovies_keywords integer primary key,
+    idmovies integer not null references Movies(idmovies),
+    idkeywords integer not null references Keywords(idkeywords),
+    idseries integer 
 );
 
-CREATE TABLE Movies_Info (
-    idmovies_info varchar(50) primary key,
-    idmovies varchar(50) not null references Movies(idmovies),
-    idmovies_keywords varchar(50) not null references Movies_keywords(idmovies_keywords) 
-);
 
 CREATE TABLE Cast_ ( 
-    idcast varchar(50) primary key,
-    idmovies varchar(50) not null references Movies(idmovies), 
-    idseries varchar(50) references Series(idseries),
-    idactors varchar(50) not null references Actors(idactors),
+    idcast integer primary key,
+    idmovies integer not null references Movies(idmovies), 
+    idactors integer not null references Actors(idactors),
     character varchar(2000), 
     billing_position varchar(50)
 );
