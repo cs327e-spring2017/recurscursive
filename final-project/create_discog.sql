d8d3cd150f393ed5ac31168690e4981c4bc0d453
Khalid Hamza <Khalid_hamza@utexas.edu>
2017-04-06 20:05:07 -0500

    Update create_discog.sql

--- final-project/create_discog.sql
+++ final-project/create_discog.sql
@@ -20,7 +20,7 @@ create table Labels(
 
 create table Releases(
    release_id integer,
-   title varchar(100),
+   title varchar(1000),
    num_tracks integer,
    released varchar(1000),
    country varchar(1000)
