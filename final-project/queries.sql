4b2354eae5bd8dc647a5a4f5808f417e59a9c2b5
eaatx10 <elias.ansari@utexas.edu>
2017-04-15 17:56:45 -0500

    updating queries

--- final-project/queries.sql
+++ final-project/queries.sql
@@ -15,7 +15,7 @@ SELECT COUNT(MB_Release.id)
 FROM MB_Release 
 JOIN MB_Release_Packaging ON (MB_Release_Packaging.id = MB_Release.packaging) 
 JOIN MB_Release_Status ON (MB_Release_Status.id = MB_Release.status) 
-WHERE MB_Release_Packaging.name = 'Digipak' AND MB_Release_Status.name = 'Official'; */
+WHERE MB_Release_Packaging.name = 'Digipak' AND MB_Release_Status.name = 'Official';
 
 --3
 SELECT COUNT(MB_Medium.id)
@@ -29,61 +29,53 @@ FROM D_Releases
 JOIN D_Releases_Artists ON (D_Releases_Artists.release_id = D_Releases.release_id)
 JOIN D_Artists ON (D_Releases_Artists.artist_id = D_Artists.tist_id)
 GROUP BY D_Releases.release_id
-ORDER BY COUNT(D_Releases.num_tracks) DESC LIMIT 1;
+ORDER BY COUNT(D_Releases.num_tracks) DESC LIMIT 1; 
+
 
 --5
 SELECT MB_Area.name
 FROM MB_Area
-JOIN MB_Label ON (MB_Area = MB_Label) <- need an attribute
+JOIN MB_Label ON (MB_Area.id = MB_Label.area) 
 JOIN D_Labels ON (MB_Label.name = D_Labels.name)
 JOIN D_Releases_Labels ON (D_Labels.label_id = D_Releases_Labels.label_id)
 JOIN D_Releases ON (D_Releases_Labels.release_id = D_Releases.release_id)
-Where D_Releases.title = "Moonchildren EP"
+WHERE D_Releases.title = 'Stalker'; 
 
 --6
-SELECT COUNT(MB_Release.id)
-FROM MB_Release
-JOIN MB_Release_Unknown_Country ON (MB_Release_Unknown_Country.release = MB_Release.id)
-GROUP BY MB_Release_Unknown_Country HAVING MB_Release
-
-
---7
 SELECT COUNT(MB_Artist.id)
 FROM MB_Artist
 JOIN MB_Artist_Type ON (MB_Artist.type = MB_Artist_Type.id)
 JOIN MB_Area ON (MB_Artist.area = MB_Area.id)
-JOIN MB_Area_Type ON (MB_Area.type = MB_Area_Type.id)
-WHERE MB_Area_Type.name = "Studio" AND MB_Artist_Type.name = "orchestrial"
+JOIN MB_Place ON (MB_Place.area = MB_Area.id)
+JOIN MB_Place_Type ON (MB_Place_Type.id = MB_Place.type)
+WHERE MB_Place_Type.name = 'Studio' AND MB_Artist_Type.name = 'Group';
 
---8
-SELECT COUNT(play_count)
+
+--7
+SELECT COUNT(MS_Songs_Popularity.play_count)
 FROM MS_Songs_Popularity 
 JOIN MS_Songs_Summary ON (MS_Songs_Summary.song_id = MS_Songs_Popularity.song)
-where MS_Songs_Summary.year = 2005
+where MS_Songs_Summary.year = 1988 AND MS_Songs_Summary.title = 'Passion Protein';
 
---9
+--8
 SELECT MS_Artist_Mbtag.mbtag, MS_Artist_Term.term
 FROM MS_Artist_Mbtag 
 JOIN MS_Artist_Term ON (MS_Artist_Mbtag.artist_id = MS_Artist_Term.artist_id)
-WHERE MS_Artist_Mbtag.artist_id = 111222333
+WHERE MS_Artist_Mbtag.artist_id = 'AR00P3M1187FB5428C';
 
---10
+--9
 SELECT COUNT(D_Releases.release_id)
 FROM D_Releases 
 JOIN D_Releases_Genres ON (D_Releases.release_id = D_Releases_Genres.release_id)
 JOIN D_Genres ON (D_Releases_Genres.genre_id = D_Genres.genre_id)
+WHERE D_Genres.name = 'Rock';
 
---11
+--10
 SELECT COUNT(MB_Artist.id)
 FROM MB_Artist
-JOIN MB_Gender ON (MB_Artist = MB_Gender) <- needs attributes
+JOIN MB_Gender ON (MB_Artist.gender = MB_Gender.id) 
 JOIN MB_Area ON (MB_Artist.area = MB_Area.id)
 JOIN MB_Area_Type ON (MB_Area.type = MB_Area_Type.id)
-WHERE MB_Gender.name = "Female" AND MB_Area_Type.name = "island"
+WHERE MB_Gender.name = 'Female' AND MB_Area_Type.name = 'Island';
 
-12) In what year was a given song released?
---Maybe change to the most popular song in a given year?
-SELECT MS_Songs_Summary.year
-FROM MS_Songs_Summary 
-JOIN MB_Recording ON (MB_Recording.name = MS_Songs_Summary.title)
-WHERE MB_Recording.name = "Soar"
+
