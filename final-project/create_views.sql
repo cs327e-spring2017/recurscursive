8d02c342f4913f05bcecc0dbddee84a320830be0
eaatx10 <elias.ansari@utexas.edu>
2017-04-19 18:21:07 -0500

    updating create_views.sql

--- final-project/create_views.sql
+++ final-project/create_views.sql
@@ -1,6 +1,6 @@
 set search_path = unified;
 
---1
+-- Time: 2584.113 ms
 create or replace view v_artist_credits_recordings as SELECT MB_Artist_Credit_Name.name 
 FROM MB_Recording 
 JOIN MB_Artist_Credit 
@@ -11,24 +11,20 @@ GROUP BY MB_Artist_Credit_Name.name
 ORDER BY COUNT(ref_count) DESC LIMIT 1;
 
 
---2
+-- Time: 183.416 ms
 create or replace view v_digipak_releases as SELECT COUNT(MB_Release.id)
 FROM MB_Release 
 JOIN MB_Release_Packaging ON (MB_Release_Packaging.id = MB_Release.packaging) 
 JOIN MB_Release_Status ON (MB_Release_Status.id = MB_Release.status) 
 WHERE MB_Release_Packaging.name = 'Digipak' AND MB_Release_Status.name = 'Official';
 
-
-
---3
+-- Time: 158.538 ms
 create or replace view v_cassette_format as SELECT COUNT(MB_Medium.id) 
 FROM MB_Medium
 JOIN MB_Medium_Format ON (MB_Medium_Format.id = MB_Medium.format)
 WHERE MB_Medium_Format.year = 1964 AND MB_Medium_Format.name = 'Cassette';
 
-
-
---4
+-- Time: 1659.737 ms
 create or replace view v_albums_released as SELECT COUNT(D_Releases.release_id)
 FROM D_Releases
 JOIN D_Releases_Artists ON (D_Releases_Artists.release_id = D_Releases.release_id)
@@ -36,9 +32,7 @@ JOIN D_Artists ON (D_Releases_Artists.artist_id = D_Artists.tist_id)
 GROUP BY D_Releases.release_id
 ORDER BY COUNT(D_Releases.num_tracks) DESC LIMIT 1; 
 
-
-
---5
+-- Time: 632.831 ms
 create or replace view v_area_stalker_album as SELECT MB_Area.name
 FROM MB_Area
 JOIN MB_Label ON (MB_Area.id = MB_Label.area) 
@@ -47,9 +41,7 @@ JOIN D_Releases_Labels ON (D_Labels.label_id = D_Releases_Labels.label_id)
 JOIN D_Releases ON (D_Releases_Labels.release_id = D_Releases.release_id)
 WHERE D_Releases.title = 'Stalker'; 
 
-
-
---6
+-- Time: 273.063 ms
 create or replace view v_group_studio as SELECT COUNT(MB_Artist.id)
 FROM MB_Artist
 JOIN MB_Artist_Type ON (MB_Artist.type = MB_Artist_Type.id)
@@ -58,32 +50,26 @@ JOIN MB_Place ON (MB_Place.area = MB_Area.id)
 JOIN MB_Place_Type ON (MB_Place_Type.id = MB_Place.type)
 WHERE MB_Place_Type.name = 'Studio' AND MB_Artist_Type.name = 'Group';
 
-
-
---7
+-- Time: 4046.305 ms
 create or replace view v_play_count_year as SELECT COUNT(MS_Songs_Popularity.play_count)
 FROM MS_Songs_Popularity 
 JOIN MS_Songs_Summary ON (MS_Songs_Summary.song_id = MS_Songs_Popularity.song)
 where MS_Songs_Summary.year = 1988 AND MS_Songs_Summary.title = 'Passion Protein';
 
-
-
---8
+-- Time: 172.594 ms
 create or replace view v_artist_mbtag_term as SELECT MS_Artist_Mbtag.mbtag, MS_Artist_Term.term
 FROM MS_Artist_Mbtag 
 JOIN MS_Artist_Term ON (MS_Artist_Mbtag.artist_id = MS_Artist_Term.artist_id)
 WHERE MS_Artist_Mbtag.artist_id = 'AR00P3M1187FB5428C';
 
-
---9
+-- Time: 258.434 ms
 create or replace view v_us_rock as SELECT COUNT(D_Releases.release_id)
 FROM D_Releases 
 JOIN D_Releases_Genres ON (D_Releases.release_id = D_Releases_Genres.release_id)
 JOIN D_Genres ON (D_Releases_Genres.genre_id = D_Genres.genre_id)
 WHERE D_Genres.name = 'Rock';
 
-
---10
+-- Time: 200.670 ms
 create or replace view v_female_islands as SELECT COUNT(MB_Artist.id)
 FROM MB_Artist
 JOIN MB_Gender ON (MB_Artist.gender = MB_Gender.id) 
